f40e2c871807102df7f53eaec2fc34dc840bcd83
Sergio Lopes <sergiooliveiralopes@yahoo.com.br>
2020-04-02 15:57:22 -0300

    agora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent } from './app.component'
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
